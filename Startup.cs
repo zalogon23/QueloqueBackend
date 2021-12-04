5b50bbff019ecfc6d647003b28aa50c40a201990
Gonzalo Zarate <gzarate@hexacta.com>
2021-12-04 16:39:03 -0300

    Allowed other ip address to connect

--- Startup.cs
+++ Startup.cs
@@ -22,100 +22,100 @@ using System.Text;
 
 namespace backend
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddCors(o =>
-      {
-        o.AddPolicy("AllowAll", builder =>
+        public Startup(IConfiguration configuration)
         {
-          builder
-          .WithOrigins("http://192.168.0.2:19006", "http://localhost:19006")
-          .AllowAnyHeader()
-          .AllowAnyMethod()
-          .AllowCredentials();
-        });
-      });
+            Configuration = configuration;
+        }
 
-      services.Configure<JWTOptions>(Configuration.GetSection(nameof(JWTOptions)));
-      services.AddSingleton<IJWTOptions>(sp => sp.GetRequiredService<IOptions<JWTOptions>>().Value);
+        public IConfiguration Configuration { get; }
 
-      var secretKey = Configuration.GetValue<string>($"{nameof(JWTOptions)}:SecretKey");
-
-      services
-      .AddAuthentication(options =>
-      {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-      })
-      .AddJwtBearer(options =>
-      {
-        options.TokenValidationParameters = new TokenValidationParameters
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-          ValidateIssuer = false,
-          ValidateAudience = false,
-          ValidateIssuerSigningKey = true,
-          ValidateLifetime = true,
-          ClockSkew = TimeSpan.Zero,
-          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
-        };
-      });
+            services.AddCors(o =>
+            {
+                o.AddPolicy("AllowAll", builder =>
+          {
+                  builder
+            .WithOrigins("http://192.168.0.2:19006", "http://192.168.0.43:19006", "http://localhost:19006")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+              });
+            });
 
-      services.Configure<MongoDbOptions>(Configuration.GetSection(nameof(MongoDbOptions)));
-      services.AddSingleton<IMongoDbOptions>(sp => sp.GetRequiredService<IOptions<MongoDbOptions>>().Value);
+            services.Configure<JWTOptions>(Configuration.GetSection(nameof(JWTOptions)));
+            services.AddSingleton<IJWTOptions>(sp => sp.GetRequiredService<IOptions<JWTOptions>>().Value);
 
-      services.AddSingleton<MongoClient>(sp =>
-      {
-        var mongoDbOptions = sp.GetRequiredService<IMongoDbOptions>();
-        var mongoClient = new MongoClient(mongoDbOptions.ConnectionString);
-        return mongoClient;
-      });
+            var secretKey = Configuration.GetValue<string>($"{nameof(JWTOptions)}:SecretKey");
 
-      services.AddSingleton<SessionsServices>();
-      services.AddSingleton<AuthenticationTokensServices>();
-      services.AddSingleton<UsersServices>();
+            services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
 
-      services.AddSignalR();
-      services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "backend", Version = "v1" });
-      });
+            services.Configure<MongoDbOptions>(Configuration.GetSection(nameof(MongoDbOptions)));
+            services.AddSingleton<IMongoDbOptions>(sp => sp.GetRequiredService<IOptions<MongoDbOptions>>().Value);
+
+            services.AddSingleton<MongoClient>(sp =>
+            {
+                var mongoDbOptions = sp.GetRequiredService<IMongoDbOptions>();
+                var mongoClient = new MongoClient(mongoDbOptions.ConnectionString);
+                return mongoClient;
+            });
+
+            services.AddSingleton<SessionsServices>();
+            services.AddSingleton<AuthenticationTokensServices>();
+            services.AddSingleton<UsersServices>();
+
+            services.AddSignalR();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "backend", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseCors("AllowAll");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chatHub");
+            });
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-
-      app.UseCors("AllowAll");
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend v1"));
-      }
-
-      app.UseHttpsRedirection();
-
-      app.UseRouting();
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-        endpoints.MapHub<ChatHub>("/chatHub");
-      });
-    }
-  }
 }
